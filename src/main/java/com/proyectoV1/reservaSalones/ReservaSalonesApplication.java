8d42fe82d37fe32a3d87257f455d61274bc2faee
Jean Pool <poolgamer0920u@gmail.com>
2023-11-28 15:28:00 -0400

    primer commit

---
+++ src/main/java/com/proyectoV1/reservaSalones/ReservaSalonesApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoV1.reservaSalones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoV1/reservaSalones/ReservaSalonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoV1.reservaSalones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSalonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
